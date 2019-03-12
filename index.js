2cca8d2855be8254b0ec6c0ce315fabb1a7d801a
Thiago Miranda <mirandathiago@gmail.com>
2019-03-12 15:11:21 -0300

    Add search term and prefix

---
+++ index.js
@@ -0,0 +1,25 @@
+const readLine = require('readline-sync');
+
+function start(){
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+
+    function askAndReturnSearchTerm(){
+        return readLine.question('Type a Wikipedia search term: ')
+    }
+    function askAndReturnPrefix(){
+        const prefixes = ['Who is','What is','The history of']
+        const selectedPrefixIndex = readLine.keyInSelect(prefixes,'Choose one Option');
+        
+        return prefixes[selectedPrefixIndex];
+    
+    }
+
+    console.log(content);
+
+}
+
+start()
